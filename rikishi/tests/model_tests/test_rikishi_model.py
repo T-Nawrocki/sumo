32cf5937230b83e3af12d171eba0e117515ab6fb
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-02 19:44:55 +0100

    SM-5: Added basic pytest setup and tests for basic rikishi model fields

---
+++ rikishi/tests/model_tests/test_rikishi_model.py
@@ -0,0 +1,17 @@
+import datetime
+
+import pytest
+
+from rikishi.models.rikishi import Rikishi
+
+
+@pytest.mark.django_db
+class TestRikishi:
+    def test_rikishi_model_fields(self):
+        rikishi = Rikishi.objects.get(id=1)
+        assert rikishi.name_first == "Hakuho"
+        assert rikishi.name_second == "Sho"
+        assert rikishi.birth_name == "Monkhbatyn Davaajargal"
+        assert rikishi.date_of_birth == datetime.date(1985, 3, 11)
+        assert rikishi.height == 192
+        assert rikishi.weight == 158
