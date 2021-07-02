21f5804673610a891e7c1d42789922932fae57b8
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-02 19:43:13 +0100

    SM-5: Added Rikishi model with basic fields

--- rikishi/apps.py
+++ rikishi/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RikishiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rikishi'
+
+    def ready(self):
+        from rikishi.models import rikishi
---
+++ rikishi/models/rikishi.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Rikishi(models.Model):
+    """
+        A rikishi (sumo wrestler).
+
+        Rikishi are one of the core objects for the application.
+        Rikishi belong to a Heya and come from a Shusshin, which both act primarily
+        as collections of Rikishi.
+    """
+
+    # MODEL FIELDS
+    name_first = models.CharField(max_length=255)
+    name_second = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    birth_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+
+
+
