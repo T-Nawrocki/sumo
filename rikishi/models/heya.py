2b46b33a483d736721d8bed2768028771a6e3611
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-02 21:28:26 +0100

    SM-8: Added is_active field to heya and squashed migrations

--- rikishi/models/heya.py
+++ rikishi/models/heya.py
@@ -5,3 +5,4 @@ class Heya (models.Model):
     """A heya (stable). Primarily acts as a collection of Rikishi."""
 
     name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
