921de671c2dfd4fe3a61c172c22aa0f0090de073
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-02 22:13:22 +0100

    SM-8: Added tests for basic rikishi app relationships

--- rikishi/tests/model_tests/test_heya_model.py
+++ rikishi/tests/model_tests/test_heya_model.py
@@ -1,11 +1,17 @@
 import pytest
 
 from rikishi.models.heya import Heya
+from rikishi.models.rikishi import Rikishi
 
 
 @pytest.mark.django_db
 class TestHeya:
-    def test_heya_model_fields(self):
+    def test_basic_heya_model_fields(self):
         heya = Heya.objects.get(id=1)
         assert heya.name == "Miyagino"
         assert heya.is_active
+
+    def test_heya_has_rikishi_set(self):
+        heya = Heya.objects.get(id=1)
+        rikishi = Rikishi.objects.get(id=1)
+        assert heya.rikishi_set.first() == rikishi
