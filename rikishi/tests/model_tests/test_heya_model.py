3c2f4c3b8aa64d4d3ad9be569af9522453709907
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-03 16:35:48 +0100

    SM-10: Added tests for Heya name validation and cleaning

--- rikishi/tests/model_tests/test_heya_model.py
+++ rikishi/tests/model_tests/test_heya_model.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 from rikishi.models.heya import Heya
 from rikishi.models.rikishi import Rikishi
@@ -6,12 +7,34 @@ from rikishi.models.rikishi import Rikishi
 
 @pytest.mark.django_db
 class TestHeya:
+
+    # MODEL FIELDS
     def test_basic_heya_model_fields(self):
         heya = Heya.objects.get(id=1)
-        assert heya.name == "Miyagino"
+        assert heya.name == "miyagino"
         assert heya.is_active
 
+    # RELATIONSHIPS
     def test_heya_has_rikishi_set(self):
         heya = Heya.objects.get(id=1)
         rikishi = Rikishi.objects.get(id=1)
         assert heya.rikishi_set.first() == rikishi
+
+    # CLEANING
+    def test_name_is_cleaned_to_lowercase(self):
+        heya = Heya.objects.create(name='Kise')
+        assert heya.name == "kise"
+
+    def test_cannot_create_heya_with_same_name_as_another_active_heya(self):
+        heya = Heya(name='miyagino')
+        with pytest.raises(ValidationError) as excinfo:
+            heya.save()
+        assert 'An active Heya already exists with that name.' in str(excinfo.value)
+
+    def test_can_create_heya_with_same_name_as_inactive_heya(self):
+        heya = Heya(name='araiso')
+        heya.save()
+
+    def test_can_create_inactive_heya_with_same_name_as_active_heya(self):
+        heya = Heya(name='miyagino', is_active=False)
+        heya.save()
