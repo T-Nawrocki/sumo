9b07607c99ffbd26574e1afda96793719901425a
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-03 20:51:06 +0100

    SM-11: Added tests for Shusshin prefecture validation

--- rikishi/tests/model_tests/test_shusshin_model.py
+++ rikishi/tests/model_tests/test_shusshin_model.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 from rikishi.models.rikishi import Rikishi
 from rikishi.models.shusshin import Shusshin
@@ -6,6 +7,8 @@ from rikishi.models.shusshin import Shusshin
 
 @pytest.mark.django_db
 class TestShusshin:
+
+    # MODEL FIELDS
     def test_basic_shusshin_model_fields(self):
         shusshin = Shusshin.objects.get(id=1)
         assert shusshin.town == "sapporo"
@@ -16,3 +19,28 @@ class TestShusshin:
         shusshin = Shusshin.objects.get(id=1)
         rikishi = Rikishi.objects.get(id=1)
         assert shusshin.rikishi_set.first() == rikishi
+
+    # CLEANING
+    def test_cannot_create_shusshin_with_invalid_country(self):
+        shusshin = Shusshin(town='realtown', country='legitimateland')
+        with pytest.raises(ValidationError) as excinfo:
+            shusshin.save()
+        assert "Value 'legitimateland' is not a valid choice." in str(excinfo.value)
+
+    def test_foreign_shusshin_cannot_have_prefecture(self):
+        shusshin = Shusshin(town='edinburgh', prefecture='midlothian', country='GB')
+        with pytest.raises(ValidationError) as excinfo:
+            shusshin.save()
+        assert 'Foreign Shusshin cannot have a prefecture.' in str(excinfo.value)
+
+    def test_japanese_shusshin_must_have_prefecture(self):
+        shusshin = Shusshin(town='tokyo', country='JP')
+        with pytest.raises(ValidationError) as excinfo:
+            shusshin.save()
+        assert 'Shusshin in Japan must specify a prefecture.' in str(excinfo.value)
+
+    def test_cannot_create_shusshin_with_invalid_prefecture(self):
+        shusshin = Shusshin(town='edinburgh', prefecture='midlothian', country='JP')
+        with pytest.raises(ValidationError) as excinfo:
+            shusshin.save()
+        assert "Value 'midlothian' is not a valid choice." in str(excinfo.value)
