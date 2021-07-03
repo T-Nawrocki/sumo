3e6d28aa421a98de3d8e33e9ec59a6c553096255
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-03 16:35:23 +0100

    SM-10 added name cleaning and validation to Heya

--- rikishi/models/heya.py
+++ rikishi/models/heya.py
@@ -1,8 +1,26 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from common.mixins.validate_model_mixin import ValidateModelMixin
 
-class Heya (models.Model):
+
+class Heya (ValidateModelMixin, models.Model):
     """A heya (stable). Primarily acts as a collection of Rikishi."""
 
+    # MODEL FIELDS
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+
+    # CLEANING
+    def _validate_name_is_unique(self):
+        matching_heya_exists = Heya.objects.exclude(id=self.id).filter(
+            is_active=True,
+            name=self.name,
+        ).exists()
+        if matching_heya_exists:
+            raise ValidationError('An active Heya already exists with that name.')
+
+    def clean(self):
+        self.name = self.name.lower()
+        if self.is_active:
+            self._validate_name_is_unique()
