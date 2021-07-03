5dc56cd9a1b6ed47f486647aa8d8fb0dce62a2d5
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-03 20:50:46 +0100

    SM-11: Added prefecture validation to Shusshin

--- rikishi/models/shusshin.py
+++ rikishi/models/shusshin.py
@@ -1,12 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from common.mixins.validate_model_mixin import ValidateModelMixin
 from geography.countries import COUNTRIES
 from geography.prefectures import PREFECTURES
 
 
-class Shusshin (models.Model):
+class Shusshin (ValidateModelMixin, models.Model):
     """A shusshin (place of origin). Primarily acts as a collection of Rikishi."""
 
+    # MODEL FIELDS
     town = models.CharField(max_length=255)
     prefecture = models.CharField(
         max_length=255,
@@ -18,3 +21,13 @@ class Shusshin (models.Model):
         choices=COUNTRIES,
         default='JP'
     )
+
+    # CLEANING
+    def _validate_prefecture(self):
+        if self.country != 'JP' and self.prefecture:
+            raise ValidationError('Foreign Shusshin cannot have a prefecture.')
+        if self.country == 'JP' and not self.prefecture:
+            raise ValidationError('Shusshin in Japan must specify a prefecture.')
+
+    def clean(self):
+        self._validate_prefecture()
