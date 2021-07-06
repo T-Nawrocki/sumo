df7afc3f8530021286439dc0caa09a2d49428162
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-06 20:53:54 +0100

    SM-15: Added __str__ method to Shusshin

--- sumo/rikishi/models/shusshin.py
+++ sumo/rikishi/models/shusshin.py
@@ -1,24 +1,32 @@
+from sumo.common.utils import choices_as_dict
 from django.core.exceptions import ValidationError
 from django.db import models
 
 from sumo.common.mixins.validate_model_mixin import ValidateModelMixin
-from sumo.geography.countries import COUNTRIES
-from sumo.geography.prefectures import PREFECTURES
+from sumo.geography import prefectures
+from sumo.geography import countries
 
 
 class Shusshin (ValidateModelMixin, models.Model):
     """A shusshin (place of origin). Primarily acts as a collection of Rikishi."""
 
+    def __str__(self):
+        if self.prefecture:
+            return prefectures.full_display_name(self.prefecture)
+        else:
+            return choices_as_dict(countries)[self.country]
+
+
     # MODEL FIELDS
     town = models.CharField(max_length=255)
     prefecture = models.CharField(
         max_length=255,
-        choices=PREFECTURES,
+        choices=prefectures.PREFECTURES,
         blank=True
     )
     country = models.CharField(
         max_length=255,
-        choices=COUNTRIES,
+        choices=countries.COUNTRIES,
         default='JP'
     )
 
