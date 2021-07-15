74c0985105e06208ced6c0d89b31a6654fcdd0a9
Tomasz Nawrocki <tomasz.nawrocki@ecometrica.com>
2021-07-15 20:57:03 +0100

    SM-27: Moved fixtures to sumo app level

--- sumo/rikishi/tests/conftest.py
+++ sumo/rikishi/tests/conftest.py
@@ -5,4 +5,4 @@ from django.core.management import call_command
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        call_command('loaddata', 'sumo/common/tests/fixtures.yaml')
+        call_command('loaddata', 'sumo/fixtures.yaml')
